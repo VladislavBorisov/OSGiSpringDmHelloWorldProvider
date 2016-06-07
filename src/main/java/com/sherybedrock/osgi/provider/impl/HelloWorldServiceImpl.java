c899d2ac291e42e1d6d0fd6a4b0ccce10065e585
vladislav.borisov <vladislav.borisov@WSBPSOF199.bpsf.bulpros.com>
2016-06-07 15:54:22 +0300

    LocalTime time = LocalTime.now();

--- src/main/java/com/sherybedrock/osgi/provider/impl/HelloWorldServiceImpl.java
+++ src/main/java/com/sherybedrock/osgi/provider/impl/HelloWorldServiceImpl.java
@@ -1,11 +1,14 @@
 package com.sherybedrock.osgi.provider.impl;
 
+import java.time.LocalTime;
+
 import com.sherybedrock.osgi.provider.able.HelloWorldService;
 
 public class HelloWorldServiceImpl implements HelloWorldService {
 
 	@Override
 	public void hello() {
-		System.out.println("Hello World ! ");
+		LocalTime time = LocalTime.now();
+		System.out.println("Hello World  :) " + "local time now is : " + time);
 	}
 }
